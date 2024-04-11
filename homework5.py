0bbe20a17dfe9a7261649a589c6dfeda79cc345f
Mag-37 <165717182+Mag-37@users.noreply.github.com>
2024-04-11 23:10:42 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['carrot', 'tomato', 'cucumber', 'onion', 'reddish', 'cabbage']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'spinach'
+print(my_list)
+
+my_dict = {'carrot': 'морковь', 'onion': 'лук', 'cabbage': 'капуста'}
+print(my_dict)
+print(my_dict['onion'])
+my_dict['cabbage'] = 'бабло'
+print(my_dict)
+my_dict['spinach'] = 'шпинат'
+print(my_dict)
